d5b6276fda08ef2800204a1029cb2f6f08975ff0
Cory O'Daniel <cory@coryodaniel.com>
2021-05-11 20:43:16 -1000

    wtf

--- cmd/bundle.go
+++ cmd/bundle.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"os"
 	"xo/src/bundles"
 
 	"github.com/spf13/cobra"
@@ -17,7 +18,7 @@ var bundleBuildCmd = &cobra.Command{
 	Use:   "build [Path to bundle.yaml]",
 	Short: "Builds bundle JSON Schemas",
 	Args:  cobra.ExactArgs(1),
-	RunE:  runBundleBuild,
+	Run:   runBundleBuild,
 }
 
 func init() {
@@ -26,27 +27,29 @@ func init() {
 	bundleBuildCmd.Flags().StringP("output", "o", ".", "Path to output directory.")
 }
 
-func runBundleBuild(cmd *cobra.Command, args []string) error {
+func runBundleBuild(cmd *cobra.Command, args []string) {
 	var err error
 	path := args[0]
 	output, err := cmd.Flags().GetString("output")
 
 	if err != nil {
-		return err
+		logger.Error("an error occurred while building bundle", zap.String("bundle", path), zap.Error(err))
+		os.Exit(1)
 	}
 
 	logger.Info("building bundle",
-		zap.String("source", path),
-		zap.String("output", output),
+		zap.String("bundle", path)
 	)
 
 	bundle, err := bundles.ParseBundle(path)
-	// TODO: log error
-	_ = err
+	if err != nil {
+		logger.Error("an error occurred while building bundle", zap.String("bundle", path), zap.Error(err))
+		os.Exit(1)
+	}
 	bundle.Build(output)
 
 	logger.Info("bundle built",
-		zap.String("source", path),
+		zap.String("bundle", path),
 		zap.String("output", output),
 	)
 
