e715146209dd31adf1fb976171dfed8bca16c250
chrisghill <chris@massdriver.cloud>
2023-09-04 22:23:33 -0600

    Add report-progress for scraping helm logs and sending events

---
+++ src/provisioners/helm/report_progress.go
@@ -0,0 +1,138 @@
+package helm
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+	"xo/src/massdriver"
+	"xo/src/telemetry"
+	"xo/src/util"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var creatingRegex = `^[a-zA-Z0-9]+\.go:[0-9]+: \[debug\] Creating ([a-zA-Z0-9]+) ([a-zA-Z0-9._\/-]+)\.{3}$`
+var readingRegex = `^[a-zA-Z0-9]+\.go:[0-9]+: \[debug\] Looks like there are no changes for ([a-zA-Z0-9]+) ([a-zA-Z0-9._\/-]+)$`
+var patchingRegex = `^[a-zA-Z0-9]+\.go:[0-9]+: \[debug\] Patching ([a-zA-Z0-9]+) ([a-zA-Z0-9._\/-]+)$`
+var deletingRegex = `^[a-zA-Z0-9]+\.go:[0-9]+: \[debug\] Deleting ([a-zA-Z0-9]+) ([a-zA-Z0-9._\/-]+)$`
+var completedRegex = `^[a-zA-Z0-9]+\.go:[0-9]+: \[debug\] Status ([a-zA-Z0-9]+) is ready: ([a-zA-Z0-9._\/-]+)$`
+var deletedRegex = `^[a-zA-Z0-9]+\.go:[0-9]+: \[debug\] Deleted ([a-zA-Z0-9]+) ([a-zA-Z0-9._\/-]+)$`
+var errorRegex = `^Error: (.*)$`
+
+func ReportProgressFromLogs(ctx context.Context, client *massdriver.MassdriverClient, deploymentId string, stream io.Reader) error {
+	_, span := otel.Tracer("xo").Start(ctx, "provisioners.helm.ReportProgressFromLogs")
+	telemetry.SetSpanAttributes(span)
+	defer span.End()
+
+	scanner := bufio.NewScanner(stream)
+
+	for scanner.Scan() {
+		// print the log to the console so its still visible
+		fmt.Println(scanner.Text())
+
+		// normally we'd pass context instead of a span (probably an antipattern) but for readability
+		// its much easier to annotate the same span, so we pass that instead
+		event, err := convertLogToMassdriverEvent(span, scanner.Text(), deploymentId)
+		if err != nil {
+			util.LogError(err, span, "an error occurred while parsing status message")
+		}
+
+		if event != nil {
+			err = client.PublishEventToSNS(event)
+			if err != nil {
+				util.LogError(err, span, "an error occurred while sending resource status to massdriver")
+			}
+		}
+	}
+
+	return nil
+}
+
+func convertLogToMassdriverEvent(span trace.Span, logLine, deploymentId string) (*massdriver.Event, error) {
+
+	matchErrorRegex, err := regexp.Match(errorRegex, []byte(logLine))
+	if matchErrorRegex && err == nil {
+		return parseErrorLog(span, logLine, deploymentId)
+	} else {
+		return parseResourceUpdateLog(span, logLine, deploymentId)
+	}
+}
+
+func parseErrorLog(span trace.Span, logLine, deploymentId string) (*massdriver.Event, error) {
+
+	re, err := regexp.Compile(errorRegex)
+	if err != nil {
+		return nil, err
+	}
+	result := re.FindStringSubmatch(logLine)
+
+	helmError := result[1]
+
+	event := massdriver.NewEvent(massdriver.EVENT_TYPE_PROVISIONER_ERROR)
+	diagnostic := new(massdriver.EventPayloadDiagnostic)
+	diagnostic.DeploymentId = deploymentId
+	diagnostic.Message = helmError
+	diagnostic.Level = "error"
+	span.RecordError(errors.New(helmError))
+	span.SetStatus(codes.Error, helmError)
+
+	event.Payload = diagnostic
+
+	return event, nil
+}
+
+func parseResourceUpdateLog(span trace.Span, logLine, deploymentId string) (*massdriver.Event, error) {
+
+	eventToRegex := map[string]string{
+		creatingRegex:  massdriver.EVENT_TYPE_RESOURCE_CREATE_RUNNING,
+		patchingRegex:  massdriver.EVENT_TYPE_RESOURCE_UPDATE_RUNNING,
+		deletingRegex:  massdriver.EVENT_TYPE_RESOURCE_DELETE_RUNNING,
+		completedRegex: massdriver.EVENT_TYPE_RESOURCE_CREATE_COMPLETED,
+		deletedRegex:   massdriver.EVENT_TYPE_RESOURCE_DELETE_COMPLETED,
+	}
+
+	for pattern, eventType := range eventToRegex {
+		matchRegex, _ := regexp.Match(pattern, []byte(logLine))
+		if !matchRegex {
+			continue
+		}
+
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			return nil, err
+		}
+		results := re.FindStringSubmatch(logLine)
+
+		resourceType := results[1]
+		resourceName := strings.TrimPrefix(results[2], "/")
+
+		// TODO remove this trim-prefix when massdriver server can accept it
+		event := massdriver.NewEvent(strings.TrimPrefix(eventType, "resource_"))
+
+		progress := new(massdriver.EventPayloadResourceProgress)
+		progress.DeploymentId = deploymentId
+		progress.ResourceName = resourceName
+		progress.ResourceType = resourceType
+		progress.ResourceId = fmt.Sprintf("%s:%s", resourceType, resourceName)
+
+		span.AddEvent("resource-update", trace.WithAttributes(
+			attribute.String("resource-status", eventType),
+			attribute.String("resource-id", progress.ResourceId),
+			attribute.String("resource-name", progress.ResourceName),
+			attribute.String("resource-type", progress.ResourceType),
+		))
+
+		event.Payload = progress
+
+		return event, nil
+	}
+
+	return nil, nil
+}
---
+++ src/provisioners/helm/report_progress_test.go
@@ -0,0 +1,79 @@
+package helm_test
+
+import (
+	"context"
+	"os"
+	"testing"
+	"xo/src/massdriver"
+	"xo/src/provisioners/helm"
+
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+var testRequests []string
+
+type SNSTestClient struct {
+	Data *string
+}
+
+func (c *SNSTestClient) Publish(ctx context.Context, params *sns.PublishInput, optFns ...func(*sns.Options)) (*sns.PublishOutput, error) {
+	testRequests = append(testRequests, *params.Message)
+	return &sns.PublishOutput{}, nil
+}
+
+func TestReportProgressFromLogs(t *testing.T) {
+	type testData struct {
+		name  string
+		input string
+		want  []string
+	}
+	tests := []testData{
+		{
+			name:  "standard",
+			input: "testdata/helm-output.log",
+			want: []string{
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"create_running"},"payload":{"deployment_id":"id","resource_name":"foo","resource_type":"Namespace","resource_id":"Namespace:foo"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"create_running"},"payload":{"deployment_id":"id","resource_name":"foo/foo-provisioner","resource_type":"Deployment","resource_id":"Deployment:foo/foo-provisioner"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"update_running"},"payload":{"deployment_id":"id","resource_name":"foo/foo-provisioner","resource_type":"Deployment","resource_id":"Deployment:foo/foo-provisioner"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"create_completed"},"payload":{"deployment_id":"id","resource_name":"foo/foo-provisioner","resource_type":"Deployment","resource_id":"Deployment:foo/foo-provisioner"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"delete_running"},"payload":{"deployment_id":"id","resource_name":"foo/foo-provisioner","resource_type":"ServiceAccount","resource_id":"ServiceAccount:foo/foo-provisioner"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"delete_completed"},"payload":{"deployment_id":"id","resource_name":"foo/foo-provisioner","resource_type":"Deployment","resource_id":"Deployment:foo/foo-provisioner"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"provisioner_error"},"payload":{"deployment_id":"id","error_message":"Chart.yaml file is missing","error_details":"","error_level":"error"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"provisioner_error"},"payload":{"deployment_id":"id","error_message":"UPGRADE FAILED: context deadline exceeded","error_details":"","error_level":"error"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"provisioner_error"},"payload":{"deployment_id":"id","error_message":"UPGRADE FAILED: error validating \"\": error validating data: ValidationError(Deployment.spec.template.spec.containers[0]): unknown field \"foo\" in io.k8s.api.core.v1.Container","error_details":"","error_level":"error"}}`,
+				`{"metadata":{"timestamp":"2021-01-01 12:00:00.1234","provisioner":"helm","event_type":"provisioner_error"},"payload":{"deployment_id":"id","error_message":"uninstall: Release not loaded: foo: release: not found","error_details":"","error_level":"error"}}`,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Setenv("MASSDRIVER_PROVISIONER", "helm")
+			massdriver.EventTimeString = func() string { return "2021-01-01 12:00:00.1234" }
+			testRequests = make([]string, 0)
+			testSNSClient := SNSTestClient{}
+			testMassdriverClient := massdriver.MassdriverClient{SNSClient: &testSNSClient, Specification: &massdriver.Specification{}}
+
+			input, err := os.Open(tc.input)
+			if err != nil {
+				t.Fatalf("%d, unexpected error", err)
+			}
+			defer input.Close()
+
+			err = helm.ReportProgressFromLogs(context.Background(), &testMassdriverClient, "id", input)
+			if err != nil {
+				t.Fatalf("%d, unexpected error", err)
+			}
+
+			if len(tc.want) != len(testRequests) {
+				t.Fatalf("want: %v, got: %v", len(tc.want), len(testRequests))
+			}
+
+			for i := 0; i < len(tc.want); i++ {
+				if tc.want[i] != testRequests[i] {
+					t.Fatalf("want: %v, got: %v", tc.want[i], testRequests[i])
+				}
+			}
+		})
+	}
+}
