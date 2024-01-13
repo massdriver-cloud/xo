5affe2280b6e18767cf06d2a95a7eb3f209e76c5
chrisghill <chris@massdriver.cloud>
2024-01-12 23:06:09 -0700

    clean up commented out code

--- src/jsonschema/validate.go
+++ src/jsonschema/validate.go
@@ -31,31 +31,3 @@ func Validate(schema, document gojsonschema.JSONLoader) (bool, error) {
 
 	return true, nil
 }
-
-// func Validate(schemaPath string, documentPath string) (bool, error) {
-// 	log.Debug().
-// 		Str("schemaPath", schemaPath).
-// 		Str("documentPath", documentPath).Msg("Validating schema.")
-
-// 	sl := Load(schemaPath)
-// 	dl := Load(documentPath)
-
-// 	result, err := gojsonschema.Validate(sl, dl)
-// 	if err != nil {
-// 		log.Error().Err(err).Msg("Validator failed.")
-// 		return false, err
-// 	}
-
-// 	if !result.Valid() {
-// 		msg := fmt.Sprintf("The document failed validation:\n\tDocument: %s\n\tSchema: %s\nErrors:\n", documentPath, schemaPath)
-// 		for _, desc := range result.Errors() {
-// 			msg = msg + fmt.Sprintf("\t- %s\n", desc)
-// 		}
-
-// 		err = errors.New(msg)
-// 		log.Error().Err(err).Msg("Validation failed.")
-// 		return false, err
-// 	}
-
-// 	return true, nil
-// }
