6aeed0696e63d6a39da5502e8e4df7fc8ab17979
chrisghill <chris@massdriver.cloud>
2024-09-15 22:42:41 -0600

    working with tests

--- cmd/bundle.go
+++ cmd/bundle.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"os"
 	"xo/src/bundle"
 	"xo/src/massdriver"
 	"xo/src/telemetry"
@@ -19,7 +20,7 @@ var bundleCmd = &cobra.Command{
 
 var bundlePullCmd = &cobra.Command{
 	Use:   "pull",
-	Short: "Pulls a bundle from S3",
+	Short: "Pulls a bundle from Massdriver",
 	RunE:  runBundlePull,
 }
 
@@ -42,8 +43,17 @@ func runBundlePull(cmd *cobra.Command, args []string) error {
 		return initErr
 	}
 
+	outFile, openErr := os.OpenFile("bundle.tar.gz", os.O_CREATE|os.O_WRONLY, 0644)
+	if openErr != nil {
+		log.Error().Err(openErr).Msg("an error occurred while initializing massdriver client")
+		span.RecordError(openErr)
+		span.SetStatus(codes.Error, openErr.Error())
+		return openErr
+	}
+	defer outFile.Close()
+
 	log.Info().Msg("pulling bundle")
-	pullErr := bundle.Pull(ctx, client)
+	pullErr := bundle.Pull(ctx, client, outFile)
 	if pullErr != nil {
 		log.Error().Err(pullErr).Msg("an error occurred while pulling bundle")
 		span.RecordError(pullErr)
--- src/bundle/pull.go
+++ src/bundle/pull.go
@@ -2,27 +2,42 @@ package bundle
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"xo/src/api"
 	"xo/src/massdriver"
 	"xo/src/telemetry"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
 )
 
-func Pull(ctx context.Context, client *massdriver.MassdriverClient) error {
+func Pull(ctx context.Context, client *massdriver.MassdriverClient, outFile io.Writer) error {
 	_, span := otel.Tracer("xo").Start(ctx, "BundlePull")
 	telemetry.SetSpanAttributes(span)
 	defer span.End()
 
-	bundleBytes, err := api.GetBundleSourceCode(client.GQLCLient, client.Specification.BundleID, client.Specification.OrganizationID)
+	bundleBytes, getErr := api.GetBundleSourceCode(client.GQLCLient, client.Specification.BundleID, client.Specification.OrganizationID)
+	if getErr != nil {
+		span.RecordError(getErr)
+		span.SetStatus(codes.Error, getErr.Error())
+		return fmt.Errorf("an error occurred while getting bundle source code: %w", getErr)
+	}
 
-	// defer outFile.Close()
-	// _, err = io.Copy(outFile, obj.Body)
-	// if err != nil {
-	// 	span.RecordError(err)
-	// 	span.SetStatus(codes.Error, err.Error())
-	// 	return err
-	// }
+	decodedBundleBytes, decodeErr := base64.StdEncoding.DecodeString(string(bundleBytes))
+	if decodeErr != nil {
+		span.RecordError(decodeErr)
+		span.SetStatus(codes.Error, decodeErr.Error())
+		return fmt.Errorf("an error occurred while decoding bundle source code: %w", decodeErr)
+	}
+
+	_, writeErr := outFile.Write(decodedBundleBytes)
+	if writeErr != nil {
+		span.RecordError(writeErr)
+		span.SetStatus(codes.Error, writeErr.Error())
+		return fmt.Errorf("an error occurred while writing bundle source code: %w", writeErr)
+	}
 
 	return nil
 }
