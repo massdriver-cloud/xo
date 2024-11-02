7c083a41e0be64feaedb0a5757b18f710ce5e172
chrisghill <chris@massdriver.cloud>
2024-11-01 22:41:48 -0600

    more deletions

--- src/massdriver/event.go
+++ src/massdriver/event.go
@@ -14,27 +14,10 @@ const EVENT_TYPE_PROVISION_FAILED string = "provision_failed"
 const EVENT_TYPE_DECOMMISSION_STARTED string = "decommission_started"
 const EVENT_TYPE_DECOMMISSION_COMPLETED string = "decommission_completed"
 const EVENT_TYPE_DECOMMISSION_FAILED string = "decommission_failed"
+
 const EVENT_TYPE_ARTIFACT_CREATED string = "artifact_created"
 const EVENT_TYPE_ARTIFACT_UPDATED string = "artifact_updated"
 const EVENT_TYPE_ARTIFACT_DELETED string = "artifact_deleted"
-
-const EVENT_TYPE_PROVISIONER_VIOLATION string = "provisioner_violation"
-
-const EVENT_TYPE_PROVISIONER_COMPLETE string = "provisioner_completed"
-const EVENT_TYPE_PROVISIONER_ERROR string = "provisioner_error"
-const EVENT_TYPE_RESOURCE_CREATE_PENDING string = "resource_create_pending"
-const EVENT_TYPE_RESOURCE_CREATE_RUNNING string = "resource_create_running"
-const EVENT_TYPE_RESOURCE_CREATE_COMPLETED string = "resource_create_completed"
-const EVENT_TYPE_RESOURCE_CREATE_FAILED string = "resource_create_failed"
-const EVENT_TYPE_RESOURCE_UPDATE_PENDING string = "resource_update_pending"
-const EVENT_TYPE_RESOURCE_UPDATE_RUNNING string = "resource_update_running"
-const EVENT_TYPE_RESOURCE_UPDATE_COMPLETED string = "resource_update_completed"
-const EVENT_TYPE_RESOURCE_UPDATE_FAILED string = "resource_update_failed"
-const EVENT_TYPE_RESOURCE_DELETE_PENDING string = "resource_delete_pending"
-const EVENT_TYPE_RESOURCE_DELETE_RUNNING string = "resource_delete_running"
-const EVENT_TYPE_RESOURCE_DELETE_COMPLETED string = "resource_delete_completed"
-const EVENT_TYPE_RESOURCE_DELETE_FAILED string = "resource_delete_failed"
-const EVENT_TYPE_RESOURCE_DRIFT_DETECTED string = "resource_drift_detected"
 
 type Event struct {
 	Metadata EventMetadata `json:"metadata"`
@@ -52,30 +35,9 @@ type EventPayloadProvisionerStatus struct {
 	DeploymentId string `json:"deployment_id"`
 }
 
-type EventPayloadResourceProgress struct {
-	DeploymentId string `json:"deployment_id"`
-	ResourceName string `json:"resource_name"`
-	ResourceType string `json:"resource_type"`
-	ResourceKey  string `json:"resource_key,omitempty"`
-	ResourceId   string `json:"resource_id,omitempty"`
-}
-
 type EventPayloadArtifact struct {
 	DeploymentId string                 `json:"deployment_id"`
 	Artifact     map[string]interface{} `json:"artifact"`
-}
-
-type EventPayloadDiagnostic struct {
-	DeploymentId string `json:"deployment_id"`
-	Message      string `json:"error_message"`
-	Details      string `json:"error_details"`
-	Level        string `json:"error_level"`
-}
-
-type EventPayloadOPAViolation struct {
-	DeploymentId string      `json:"deployment_id"`
-	Rule         string      `json:"opa_rule"`
-	Value        interface{} `json:"opa_value"`
 }
 
 var EventTimeString = time.Now().String
