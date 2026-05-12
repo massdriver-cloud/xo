27d86a4765c21502df701ee157e3d3c01111a076
chrisghill <chris@massdriver.cloud>
2026-05-12 09:47:01 -0600

    support MASSDRIVER_INSTANCE_ID to replace MASSDRIVER_PACKAGE_NAME

--- src/provisioners/terraform/backend_test.go
+++ src/provisioners/terraform/backend_test.go
@@ -8,28 +8,63 @@ import (
 )
 
 func TestGenerateJSONBackendHTTPConfig(t *testing.T) {
-	spec := massdriver.Specification{
-		DeploymentID: "depId",
-		Token:        "token",
-		PackageName:  "pkg-id-long-0000",
-		URL:          "https://foo.massdriver.cloud",
-	}
-	got, _ := GenerateJSONBackendHTTPConfig(&spec, "step")
-	want := `
-	{
-		"terraform": {
-			"backend": {
-				"http": {
-					"username": "depId",
-					"password": "token",
-					"address": "https://foo.massdriver.cloud/state/pkg-id-long/step",
-					"lock_address": "https://foo.massdriver.cloud/state/pkg-id-long/step",
-					"unlock_address": "https://foo.massdriver.cloud/state/pkg-id-long/step"
+	tests := []struct {
+		name string
+		spec massdriver.Specification
+		step string
+		want string
+	}{
+		{
+			name: "generates backend from Package Name",
+			spec: massdriver.Specification{
+				DeploymentID: "depId",
+				Token:        "token",
+				PackageName:  "pkg-id-long-0000",
+				URL:          "https://foo.massdriver.cloud",
+			},
+			step: "step",
+			want: `{
+				"terraform": {
+					"backend": {
+						"http": {
+							"username": "depId",
+							"password": "token",
+							"address": "https://foo.massdriver.cloud/state/pkg-id-long/step",
+							"lock_address": "https://foo.massdriver.cloud/state/pkg-id-long/step",
+							"unlock_address": "https://foo.massdriver.cloud/state/pkg-id-long/step"
+						}
+					}
 				}
-			}
-		}
+			}`,
+		}, {
+			name: "generates backend config from Instance ID",
+			spec: massdriver.Specification{
+				DeploymentID: "depId",
+				Token:        "token",
+				InstanceID:   "inst-ance-id",
+				URL:          "https://foo.massdriver.cloud",
+			},
+			step: "step",
+			want: `{
+				"terraform": {
+					"backend": {
+						"http": {
+							"username": "depId",
+							"password": "token",
+							"address": "https://foo.massdriver.cloud/state/inst-ance-id/step",
+							"lock_address": "https://foo.massdriver.cloud/state/inst-ance-id/step",
+							"unlock_address": "https://foo.massdriver.cloud/state/inst-ance-id/step"
+						}
+					}
+				}
+			}`,
+		},
 	}
-`
 
-	require.JSONEq(t, string(got), want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := GenerateJSONBackendHTTPConfig(&tt.spec, tt.step)
+			require.JSONEq(t, string(got), tt.want)
+		})
+	}
 }
